986e041c16369a352514150871985e77fae6ecfc
Milka Elias Worku <milkaelias23@gmail.com>
2025-08-21 14:46:09 -0500

    Update login test

--- automation/src/test/java/LoginTest.java
+++ automation/src/test/java/LoginTest.java
@@ -3,15 +3,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 
-public class TestLogin {
+public class LoginTest {
     private WebDriver driver;
 
-    @BeforeMethod
+    @BeforeClass
     public void setup() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -53,7 +51,7 @@ public class TestLogin {
         Assert.assertEquals(errorMessage.getText(), expectedMessage, "The error message is incorrect.");
     }
 
-    @AfterMethod
+    @AfterClass
     public void tearDown() {
         if (driver != null) {
             driver.quit();
